814f24ffc7f816b91bdb9ab93fb5bd7662c299cb
Juliana Mota <julianamota89@gmail.com>
2019-05-06 23:21:19 +0200

    correct svgs and do event on svg icons

--- js/script.js
+++ js/script.js
@@ -10,7 +10,140 @@ function loadTimelineSVG() {
       //console.log(svgData);
 
       document.querySelector("#time-line-svg").innerHTML = svgData;
+
+      const iconsArray = document.querySelectorAll(".btn-Icon");
+      console.log(iconsArray);
+      iconsArray.forEach(eachIcon);
     });
+}
+
+function eachIcon(icon) {
+  console.log(icon);
+  icon.addEventListener("click", iconClicked);
+}
+
+function iconClicked(event) {
+  console.log(event.target.classList);
+  if (event.target.classList.value === "girlCircle") {
+    document.querySelector("#Dreamer").classList.remove("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "scoutCircle") {
+    document.querySelector("#Friendly").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "artCircle") {
+    document.querySelector("#Crealive").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "archCircle") {
+    document.querySelector("#Realistic").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "heartCircle") {
+    document.querySelector("#Passionate").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "danCircle") {
+    document.querySelector("#Adventurous").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "workCircle") {
+    document.querySelector("#Perseverant").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "searchCircle") {
+    document.querySelector("#Hopeful").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "mmdCircle") {
+    document.querySelector("#Inspiring").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Vision").classList.add("hide");
+  }
+  if (event.target.classList.value === "futureCircle") {
+    document.querySelector("#Vision").classList.remove("hide");
+    document.querySelector("#Dreamer").classList.add("hide");
+    document.querySelector("#Friendly").classList.add("hide");
+    document.querySelector("#Crealive").classList.add("hide");
+    document.querySelector("#Realistic").classList.add("hide");
+    document.querySelector("#Passionate").classList.add("hide");
+    document.querySelector("#Adventurous").classList.add("hide");
+    document.querySelector("#Perseverant").classList.add("hide");
+    document.querySelector("#Hopeful").classList.add("hide");
+    document.querySelector("#Inspiring").classList.add("hide");
+  }
 }
 
 function loadIllustrations() {
@@ -23,22 +156,22 @@ function loadIllustrations() {
 
         clone = document.querySelector("template").content.cloneNode(true);
 
+        clone.querySelector(".IllustrationBox").id = obj.title;
+
         const dataFields = clone.querySelectorAll("[data-field]");
         dataFields.forEach(element => {
-          console.log(element);
+          //console.log(element);
           const property = element.dataset.field;
-          //console.log(property);
-          // element.textContent = obj[field];
 
           if (element.dataset.field === "image") {
             element.src = "../images/" + obj[property];
             const imageI = "../images/" + obj[property];
-            console.log(imageI);
+            //console.log(imageI);
           } else if (element.dataset.field === "svg") {
             fetch("../svg/" + obj[property])
               .then(res => res.text())
               .then(svgData => {
-                console.log(svgData);
+                //console.log(svgData);
 
                 element.innerHTML = svgData;
               });
@@ -48,6 +181,14 @@ function loadIllustrations() {
         });
 
         document.querySelector("#illustration-svg").appendChild(clone);
+      });
+      const illustrationSections = document.querySelectorAll(
+        ".IllustrationBox"
+      );
+      //console.log(illustrationSections);
+      illustrationSections.forEach(section => {
+        //console.log(section);
+        section.classList.add("hide");
       });
     });
 }

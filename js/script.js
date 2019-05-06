7ce2b41b1a156e7c6bc767953259ad778ed9a892
Juliana Mota <julianamota89@gmail.com>
2019-05-06 12:13:05 +0200

    svg correct and script fetch json

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
 window.addEventListener("DOMContentLoaded", loadTimelineSVG);
+window.addEventListener("DOMContentLoaded", loadIllustrations);
 
 function loadTimelineSVG() {
-  console.log("");
+  //console.log("");
 
   fetch("../svg/timeline.svg")
     .then(response => response.text())
     .then(svgData => {
-      console.log(svgData);
+      //console.log(svgData);
 
       document.querySelector("#time-line-svg").innerHTML = svgData;
+    });
+}
+
+function loadIllustrations() {
+  fetch("js/data.json")
+    .then(res => res.json())
+    .then(dataAr => {
+      //console.log(dataAr);
+      dataAr.forEach(obj => {
+        //console.log(obj);
+
+        clone = document.querySelector("template").content.cloneNode(true);
+
+        const dataFields = clone.querySelectorAll("[data-field]");
+        dataFields.forEach(element => {
+          console.log(element);
+          const property = element.dataset.field;
+          //console.log(property);
+          // element.textContent = obj[field];
+
+          if (element.dataset.field === "image") {
+            element.src = "../images/" + obj[property];
+            const imageI = "../images/" + obj[property];
+            console.log(imageI);
+          } else if (element.dataset.field === "svg") {
+            fetch("../svg/" + obj[property])
+              .then(res => res.text())
+              .then(svgData => {
+                console.log(svgData);
+
+                element.innerHTML = svgData;
+              });
+          } else {
+            element.textContent = obj[property];
+          }
+        });
+
+        document.querySelector("#illustration-svg").appendChild(clone);
+      });
     });
 }
